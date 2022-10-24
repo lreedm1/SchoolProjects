ce4bf568ac22e1040fb4b222ed36fabd9662748d
lreedm1 <lreedm1@gmail.com>
2022-10-23 22:16:18 -0500

    add javadoc headers and remove app.java

--- Dragon Treasure Adventure 2.0/src/Player.java
+++ Dragon Treasure Adventure 2.0/src/Player.java
@@ -1,3 +1,28 @@
+//////////////// FILE HEADER (INCLUDE IN EVERY FILE) //////////////////////////
+//
+// Title:    Dragon Treasure Adventure 2.0
+// Course:   CS 300 Fall 2022
+//
+// Author:   Reed Lokken
+// Email:    rlokken@wisc.edu
+// Lecturer: Hobbes LeGault
+//
+//////////////////// PAIR PROGRAMMERS COMPLETE THIS SECTION ///////////////////
+//
+// Partner Name: Aarav Gupta
+// Partner Email: agupta297@wisc.edu
+// Partner Lecturer's Name: Hobbes LeGault
+// 
+// VERIFY THE FOLLOWING BY PLACING AN X NEXT TO EACH TRUE STATEMENT:
+//   X Write-up states that pair programming is allowed for this assignment.
+//   X We have both read and understand the course Pair Programming Policy.
+//   X We have registered our team prior to the team registration deadline.
+//
+///////////////////////// ALWAYS CREDIT OUTSIDE HELP //////////////////////////
+//
+// Persons: NONE        
+// Online Sources: Github Copilot
+///////////////////////////////////////////////////////////////////////////////
 import java.util.ArrayList;
 
 /** @author Michelle
