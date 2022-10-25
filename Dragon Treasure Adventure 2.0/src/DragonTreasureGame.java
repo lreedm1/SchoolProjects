78d06aad268b9f127de40da819d4b0903dbd5942
Aarav Gupta <aaravgupta@Aaravs-MacBook-Pro.local>
2022-10-25 00:25:50 -0500

    updated

--- Dragon Treasure Adventure 2.0/src/DragonTreasureGame.java
+++ Dragon Treasure Adventure 2.0/src/DragonTreasureGame.java
@@ -1,16 +1,18 @@
+import java.io.File;
 import java.util.ArrayList;
 import processing.core.PApplet;
 import processing.core.PImage;
 
 public class DragonTreasureGame extends PApplet {
-	private ArrayList<Character> characters = new ArrayList<Character>();
+	private ArrayList<Character> characters;
+	private File roomInfo;
+	private File mapInfo;
 	private boolean isDragonTurn = false;
 	private int gameState= 0;
 	private ArrayList<Room>roomList;
 	public static void main(String[] args) {
 		PApplet.main("DragonTreasureGame");
 	}
-
 	@Override
 	public void settings() {
 		this.size(800,600);
@@ -18,23 +20,24 @@ public class DragonTreasureGame extends PApplet {
 	@Override
 	public void setup() {
 		this.getSurface().setTitle("Dragon Treasure Adventure"); // sets the title of the window
-		this.imageMode(PApplet.CORNER); //Images are drawn using the x,y-coordinate
-		//as the top-left corner
-		this.rectMode(PApplet.CORNERS); //When drawing rectangles interprets args
-		//as top-left corner and bottom-right corner respectively
+		this.imageMode(PApplet.CORNER); //Images are drawn using the x,y-coordinate as the top-left corner
+		this.rectMode(PApplet.CORNERS); //When drawing rectangles interprets args as top-left corner and bottom-right corner respectively
 		this.focused = true; // window will be active upon running program
 		this.textAlign(CENTER); // sets the text alignment to center
 		this.textSize(20); //sets the font size for the text
+		roomInfo = new File("roominfo.txt");
+		mapInfo = new File("map.txt");
 		roomList = new ArrayList<Room>();
+		characters= new ArrayList<Character>();
 		Room.setProcessing(this);
-		PImage img = this.loadImage("images/1.jpg");
-		 Room r = new Room(1, "test room", img);
-		 roomList.add(r);
-		 PImage img2 = this.loadImage("images/2.jpg");
-		 StartRoom s = new StartRoom(2, img2);
-		 roomList.add(s);
-
+		Room.loadCharacters();
 	}
+	private void loadCharacters() {
+		System.out.println("Adding characters...");
+		characters.add(new Character(getRoomByID(5),"KEYHOLDER"));
+		characters.add(new Player(getRoomByID(1)));
+		characters.add(new Dragon(getRoomByID(9)));
+		}
 	@Override
 	public void keyPressed(){
 
