5c1f947ef603a513b794956b79e90ec1b3e55a1a
Aarav Gupta <aaravgupta@Aaravs-MacBook-Pro.local>
2022-10-25 01:21:57 -0500

    updated

--- Dragon Treasure Adventure 2.0/src/DragonTreasureGame.java
+++ Dragon Treasure Adventure 2.0/src/DragonTreasureGame.java
@@ -1,5 +1,8 @@
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
+
 import processing.core.PApplet;
 import processing.core.PImage;
 
@@ -13,6 +16,116 @@ public class DragonTreasureGame extends PApplet {
 	public static void main(String[] args) {
 		PApplet.main("DragonTreasureGame");
 	}
+	private Room getRoomByID(int id){
+		int indexToEdit = roomList.indexOf(new Room(id,"dummy", null));
+		Room toEdit = roomList.get(indexToEdit); 
+		return toEdit;
+	  }
+	private void loadRoomInfo() {
+		System.out.println("Loading rooms...");
+		Scanner fileReader = null;
+		try {
+		  
+		  //scanner to read from file
+		  fileReader= new Scanner(roomInfo);
+		  
+		  //read line by line until none left
+		  while(fileReader.hasNext()) {
+			String nextLine = fileReader.nextLine();
+			
+			//parse info and create new room 
+			String[] parts = nextLine.split(" \\| ");
+			int ID = Integer.parseInt(parts[1].trim()); //get the room id
+			String imageName = null;
+			String description = null;
+			PImage image = null;
+			Room newRoom = null;
+			
+			if(parts.length >= 3) {
+			  imageName = parts[2].trim();
+			  image = this.loadImage("images" + File.separator + imageName);
+			 
+			}
+			
+			if(parts.length == 4) {
+			  description = parts[3].trim(); //get the room description
+			}
+	   
+			switch(parts[0].trim()) {
+			  case "S":
+				newRoom = new StartRoom(ID, image);
+				break;
+			  case "R":
+				newRoom = new Room(ID, description, image);
+				break;
+			  case "P":
+				newRoom = new PortalRoom(ID, description, image);
+				break;
+			  case "T":
+				newRoom = new TreasureRoom(ID);
+				break;
+			  default:
+				break;
+			}  
+			
+			if(newRoom != null) {
+			  roomList.add(newRoom);
+			}
+		  }
+		}catch(IOException e) { //handle checked exception
+		  e.printStackTrace();
+		}finally {
+		  if(fileReader != null)
+			fileReader.close(); //close scanner regardless of what happened for security reasons :)
+		}
+	  }
+	  
+	  /** Loads in room connections using the file stored in mapInfo
+	   *  @author Michelle 
+	   */
+	  private void loadMap() {
+		System.out.println("Loading map...");
+		Scanner fileReader = null;
+		try {
+			  //scanner to read from file
+			  fileReader= new Scanner(mapInfo);
+			  
+			//read line by line until none left
+			  while(fileReader.hasNext()) {
+				
+				//parse info
+				String nextLine = fileReader.nextLine();
+				String parts[] = nextLine.split(" ");
+				int id = Integer.parseInt(parts[0]);
+				
+				Room toEdit = getRoomByID(id); //get the room we need to update info for adjacent rooms
+				
+				//add all the rooms to the adj room list of toEdit
+				for(int i=1; i<parts.length; i++) {
+				  Room toAdjAdd = getRoomByID(Integer.parseInt(parts[i]));
+				  toEdit.addToAdjacentRooms(toAdjAdd);
+				}
+			  }
+			}catch(IOException e) { //handle checked exception
+			  e.printStackTrace();
+			}finally {    //close scanner regardless of what happened for security reasons :)
+			  if(fileReader != null)
+				fileReader.close();
+			}
+	  }
+	  
+	  /**
+	   * Get the room objected associated with the given ID.
+	   * @param id the ID of the room to retrieve
+	   * @return the Room that corresponds to that id
+	   * @author Michelle
+	   */
+	private void loadCharacters() {
+		System.out.println("Adding characters...");
+		characters.add(new Character(getRoomByID(5),"KEYHOLDER"));
+		characters.add(new Player(getRoomByID(1)));
+		characters.add(new Dragon(getRoomByID(9)));
+		}
 	@Override
 	public void settings() {
 		this.size(800,600);
@@ -30,14 +143,10 @@ public class DragonTreasureGame extends PApplet {
 		roomList = new ArrayList<Room>();
 		characters= new ArrayList<Character>();
 		Room.setProcessing(this);
-		Room.loadCharacters();
+		loadRoomInfo();
+		loadMap();
+		loadCharacters();
 	}
-	private void loadCharacters() {
-		System.out.println("Adding characters...");
-		characters.add(new Character(getRoomByID(5),"KEYHOLDER"));
-		characters.add(new Player(getRoomByID(1)));
-		characters.add(new Dragon(getRoomByID(9)));
-		}
 	@Override
 	public void keyPressed(){
 
@@ -45,6 +154,7 @@ public class DragonTreasureGame extends PApplet {
 	public void draw() {
 		roomList.get(0).draw();
 		roomList.get(1).draw();
+		
 	}
 
 }
