00e7d4f3ede516ffda328cfccd7ab608c9f3d3cc
lreedm1 <lreedm1@gmail.com>
2022-11-21 01:11:07 -0600

    Implemented Song.java & Song.java constructor test

--- P08 MusicPlayer300/src/MusicPlayerTester.java
+++ P08 MusicPlayer300/src/MusicPlayerTester.java
@@ -26,6 +26,82 @@
 import java.io.File;
 import java.util.Scanner;
 
-/** NOTES
- * All methods longer than 3 lines should have an inline comment
+/*
+ * This class tests Song.java, SongNode.java, Playlist.java, and MusicPlayer300.java
  */
+public class MusicPlayerTester {
+    /*
+    * Invokes the tester method that tests the functionality of each file
+    *
+    * @param args (unused)
+    */
+    public static void main(String[] args){
+        runAllTests();
+    }
+    
+    /*
+     * Tests the functionality of Song.java's constructor
+     * 
+     * @return true if the constructor works as expected, false otherwise
+     */
+    public static boolean testSongConstructor() {
+        // test the constructor with an invalid file
+        try {
+            Song song = new Song("someTitle", "someArtist", "someFilepath");
+            // the constructor does not throw an exception
+            System.out.println("Song constructor did not throw an IllegalArgumentException when passed an invalid file");
+            return false;
+        } catch (IllegalArgumentException e) {
+            // the constructor throws an exception as expected
+        } catch(Exception e) {
+            System.out.println("Song constructor returned " + e + " when it should have returned IllegalArgumentException");
+            return false;
+        }
+        // test a valid file with toString, getTitle, and getArtist
+        Song song = new Song("someTitle", "someArtist", "audio/1.mid");
+        String toString = song.toString();
+        System.out.println(toString);
+        //TODO: implement the toString method when .toString is written
+
+        // test the getTitle method
+        String title = song.getTitle();
+        if (!title.equals("someTitle")) {
+            System.out.println("Song.getTitle() returned " + title + " when it should have returned someTitle");
+            return false;
+        }
+
+        // test the getArtist method
+        String artist = song.getArtist();
+        if (!artist.equals("someArtist")) {
+            System.out.println("Song.getArtist() returned " + artist + " when it should have returned someArtist");
+            return false;
+        }
+
+     return true;   
+    }
+
+    /* 
+     * Prints the test result to the console
+     * 
+     * @param testName the name of the test
+     * @param passed true if the test passed, false otherwise
+     */
+    public static void evalTestResult(Boolean result, String testName){
+        if (result){
+            System.out.println("PASSED — " + testName);
+        } else {
+            System.out.println("*FAILED* — " + testName + " - See above for details");
+        }
+    }
+
+    /*
+     * Runs all tests
+     */
+    public static void runAllTests(){
+        System.out.println("\n-----------------Running Tests-----------------\n");
+        evalTestResult(testSongConstructor(), "SongConstructor");
+
+        System.out.println("\n-----------------End of Tests-----------------\n");
+    }
+    
+}
--- P08 MusicPlayer300/src/Song.java
+++ P08 MusicPlayer300/src/Song.java
@@ -24,8 +24,98 @@
 // Online Sources: Github Copilot 
 ///////////////////////////////////////////////////////////////////////////////
 import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
 
 /** NOTES
  * All methods longer than 3 lines should have an inline comment
  */
+
+ /*
+  * This class is a representation of a song
+  * and interfaces with the AudioUtility class
+  * which uses javax.sound.sampled to play audio
+  */
+public class Song{
+
+    private String title;           // title of the song
+    private String artist;          // artist of the song
+    private int duration;           // duration of the song in seconds
+    private AudioUtility audioClip; // audio clip of the song
+
+    /*
+    * Constructs the song object
+    * 
+    * @param title the song title
+    * @param artist the song artist
+    * @param filepath the song filepath
+    * 
+    * @throws IllegalArgumentException if the filepath is invalid
+    */
+    public Song(String title, String artist, String filepath) throws IllegalArgumentException{
+        this.title = title;
+        this.artist = artist;
+
+        // checks if the file exists
+        try{
+            this.audioClip = new AudioUtility(filepath);
+            this.duration = audioClip.getClipLength();
+
+        } catch (IOException e){
+            throw new IllegalArgumentException("Filepath is invalid");
+        }
+    }
+    
+    /*
+     * Returns the song artist
+     * @return the song artist
+     */
+    public String getArtist(){
+        return this.artist;
+    }
+
+    /*
+     * Returns the song titles
+     * @return the song title
+     */
+    public String getTitle(){
+        return this.title;
+    }
+
+    /*
+     * Checks if the song is playing
+     * @return true if the song is playing, false otherwise
+     */
+    public boolean isPlaying(){
+        return this.audioClip.isRunning();
+    }
+
+    /*
+     * Plays the song
+     */
+    public void play(){
+        // if the song is not ready to play, reopen the audio clip
+        // else play the song
+        while (!this.audioClip.isReadyToPlay()){
+            this.audioClip.reopenClip();
+        }
+        this.audioClip.startClip();
+    }
+
+    /*
+     * Pauses the song
+     */
+    public void pause(){
+        this.audioClip.stopClip();
+    }
+
+    /*
+     * Returns the song title, duration in minutes, duration in seconds, and artist
+     * 
+     * @return the song information in the format in the format |"someTitle" (minutes:seconds) by someArtist)|
+     */
+    public String toString(){
+        return "\"" + this.title + "\" (" + this.duration / 60 + ":" + this.duration % 60 + ") by " + this.artist;
+    }
+
+    }
