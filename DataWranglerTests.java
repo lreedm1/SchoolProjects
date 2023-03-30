04de25990cbbe82fe88e92fe66ddc920e13d1f84
Jack Minton <jbminton@wisc.edu>
2023-03-30 20:10:52 +0000

    added test classes back with comments

--- DataWranglerTests.java
+++ DataWranglerTests.java
@@ -8,6 +8,12 @@ import java.util.List;
 
 
 public class DataWranglerTests {
+	/**
+	 * This method loads all of the data from the file 
+	 * into a list and makes sure the size is correct.
+	 * Then it checks if the color of the first car in the 
+	 * arraylist is equal to black. 
+	 * */
     @Test
     public void testReadCarsFromFileAndGetColor() throws FileNotFoundException {
         List<CarInterface> cars = CarReaderInterface.readCarsFromFile("./USA_cars_datasets.csv");
@@ -17,7 +23,12 @@ public class DataWranglerTests {
 
         assertEquals("black", car.getColor());
     }
-
+	/**
+	 * This method loads all of the data from the file 
+         * into a list and makes sure the size is correct.
+	 * Then it checks the price of the second car and
+	 * makes sure its the same as it should be.
+	 * */
     @Test
     public void testReadCarsFromFileAndGetPrice() throws FileNotFoundException {
         List<CarInterface> cars = CarReaderInterface.readCarsFromFile("./USA_cars_datasets.csv");
@@ -26,7 +37,12 @@ public class DataWranglerTests {
         CarInterface car = cars.get(1);
         assertEquals(2899.0, car.getPrice());
     }
-
+	
+    	/** 
+	 * This method loads all of the data from the file
+         * into a list and makes sure the size is correct.
+	 * Then it checks the brand of second car and makes sure its correct.
+	 * */
     @Test
     public void testReadCarsFromFileAndGetBrand() throws FileNotFoundException {
         List<CarInterface> cars = CarReaderInterface.readCarsFromFile("./USA_cars_datasets.csv");
@@ -36,6 +52,11 @@ public class DataWranglerTests {
         assertEquals("dodge", car.getBrand());
     }
 
+    /**
+     * This method loads all of the data from the file
+     * into a list and makes sure the size is correct.
+     * Then it checks the fourth cars location and ensures its virginia.
+     * */
     @Test
     public void testReadCarsFromFileAndGetLocation() throws FileNotFoundException {
         List<CarInterface> cars = CarReaderInterface.readCarsFromFile("./USA_cars_datasets.csv");
@@ -45,6 +66,12 @@ public class DataWranglerTests {
         assertEquals("virginia", car.getLocation());
     }
 
+    /**
+     * This method loads all of the data from the file
+     * into a list and makes sure the size is correct.
+     * Then it checks two cars compared to each other 
+     * and makes sure that the value returned is positive.
+     * */
     @Test
     public void testReadCarsFromFileAndCompareTo() throws FileNotFoundException {
         List<CarInterface> cars = CarReaderInterface.readCarsFromFile("./USA_cars_datasets.csv");
